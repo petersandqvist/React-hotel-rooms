b93cddc93803b598a5e7285ea7a0b6e9735d3fdc
Peter Sandqvist <peter.sandqvist@brandson.se>
2019-05-12 11:17:32 +0200

    adding components

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,11 +1,9 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
 const Rooms = () => {
-  return (
-    <div>
-      Hello from Rooms
-    </div>
-  )
-}
+  return <Hero hero="roomsHero" />;
+   
+};
 
 export default Rooms
